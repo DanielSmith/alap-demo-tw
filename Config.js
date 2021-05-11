ef27c16b19953695d2ba2863c2f19536dbafea18
Daniel Smith <javajoint@gmail.com>
2021-05-10 22:07:12 -0400

    build fix

---
+++ Config.js
@@ -0,0 +1,360 @@
+/*
+ **	Config
+ */
+
+export const alapConfig = {
+  settings: {
+    listType: "ul",
+    menuTimeout: 5000,
+  },
+
+  macros: {
+    cars1: {
+      linkItems: "vwbug, bmwe36",
+      config: {
+        somekey: "somevalue",
+      },
+    },
+  },
+
+  allLinks: {
+    bmwe36: {
+      label: "BMW (E36) - Wikipedia",
+      url: "http://en.wikipedia.org/wiki/BMW_3_Series_(E36)",
+      tags: ["e36m3", "bmw", "car", "germany"],
+    },
+
+    kittyatola_yt: {
+      label: "Kitty - Atola Visuals (YouTube)",
+      url: "https://www.youtube.com/c/atolavisuals/videos",
+      cssClass: "violetclass",
+      tags: [
+        "atolavisuals",
+        "lapeople",
+        "cinematography",
+        "video",
+        "photography",
+        "techreview",
+        "youtube",
+      ],
+    },
+
+    davelee_yt: {
+      label: "Dave2D - Dave Lee (YouTube)",
+      url: "https://www.youtube.com/c/Dave2D/videos",
+      tags: ["dave2d", "video", "techreview", "youtube"],
+    },
+
+    mkbhd_tw: {
+      label: "MKBHD - Marques Brownlee (Twitter)",
+      url: "https://twitter.com/MKBHD",
+      tags: [
+        "mkbhd",
+        "nycpeople",
+        "njpeople",
+        "cinematography",
+        "techreview",
+        "video",
+        "photography",
+        "twitter",
+        "youtube",
+      ],
+    },
+
+    mkbhd_yt: {
+      label: "MKBHD - Marques Brownlee (YouTube)",
+      url: "https://twitter.com/MKBHD",
+      tags: [
+        "mkbhd",
+        "nycpeople",
+        "njpeople",
+        "cinematography",
+        "techreview",
+        "video",
+        "photography",
+        "youtube",
+      ],
+    },
+
+    casey_tw: {
+      label: "Casey Neistat (Twitter)",
+      url: "https://twitter.com/Casey",
+      tags: [
+        "caseyneistat",
+        "nycpeople",
+        "lapeople",
+        "cinematography",
+        "video",
+        "photography",
+        "youtube",
+      ],
+    },
+
+    casey_yt: {
+      label: "Casey Neistat (YouTube)",
+      url: "https://www.youtube.com/c/Caseyneistatofficial/videos",
+      tags: [
+        "caseyneistat",
+        "nycpeople",
+        "lapeople",
+        "cinematography",
+        "video",
+        "photography",
+        "youtube",
+      ],
+    },
+
+    makeartnow_yt: {
+      label: "Make. Art. Now. (YouTube)",
+      url: "https://www.youtube.com/c/MAKEARTNOWCHANNEL/videos",
+      tags: [
+        "makeartnow",
+        "diy",
+        "joshyeo",
+        "cinematography",
+        "video",
+        "photography",
+        "techreview",
+        "lapeople",
+        "youtube",
+      ],
+    },
+
+    danmace_yt: {
+      label: "Dan Mace (YouTube)",
+      url: "https://www.youtube.com/c/DanMace/videos",
+      tags: [
+        "danmace",
+        "cinematography",
+        "video",
+        "photography",
+        "sapeople",
+        "youtube",
+      ],
+    },
+
+    vwbug: {
+      label: "VW Bug",
+      url: "http://en.wikipedia.org/wiki/Volkswagen_Beetle",
+      tags: ["vw", "car", "germany"],
+    },
+
+    sierranevada: {
+      label: "Sierra Nevada",
+      url: "http://www.sierranevada.com/",
+      tags: ["beer", "ale"],
+    },
+
+    fosters: {
+      label: "Fosters Beer",
+      url: "http://www.fostersbeer.com/",
+      tags: ["australia", "beer", "lager"],
+    },
+
+    lagunitas: {
+      label: "Lagunitas Brewing Company",
+      url: "https://lagunitas.com",
+      tags: ["beer", "pilsner", "ale"],
+    },
+
+    radeberger: {
+      label: "Radeberger Pilsner",
+      url: "http://goo.gl/F7vhs",
+      tags: ["beer", "pilsner", "germany"],
+    },
+
+    acrecoffee: {
+      label: "Acre Coffee",
+      url: "http://www.acrecoffee.com/",
+      tags: ["cafe", "petaluma", "coffee"],
+    },
+
+    wsbistro: {
+      label: "Water Street Bistro",
+      url: "http://www.waterstreetbistro.net",
+      tags: ["cafe", "petaluma", "frenchfood"],
+    },
+
+    aqus: {
+      label: "Aqus Cafe",
+      url: "http://aquscafe.com/",
+      tags: ["cafe", "petaluma", "beer", "community", "coffee"],
+    },
+
+    nychighline: {
+      label: "The High Line",
+      url: "http://www.thehighline.org/",
+      tags: ["nyc", "park", "manhattan", "city"],
+    },
+
+    centralpark: {
+      label: "The Official Website of Central Park",
+      url: "http://www.centralparknyc.org",
+      tags: ["nyc", "park", "manhattan", "city"],
+    },
+
+    prospectpark: {
+      label: "Prospect Park",
+      url: "http://www.prospectpark.org/",
+      tags: ["nyc", "park", "brooklyn"],
+    },
+
+    mtainfo: {
+      label: "mta.info - transit",
+      url: "http://www.mta.info",
+      tags: ["nyc", "transit", "subway", "bus"],
+    },
+
+    brooklynbridge: {
+      label: "Brooklyn Bridge",
+      url: "http://en.wikipedia.org/wiki/Brooklyn_Bridge",
+      tags: ["nyc", "bridge", "brooklyn", "manhattan"],
+    },
+
+    manhattanbridge: {
+      label: "Manhattan Bridge",
+      url: "http://en.wikipedia.org/wiki/Manhattan_Bridge",
+      tags: ["nyc", "bridge", "manhattan"],
+    },
+
+    visitbrooklyn: {
+      label: "Visit Brooklyn",
+      url: "https://www.brooklynbridgepark.org",
+      tags: ["nyc", "brooklyn", "city"],
+    },
+
+    brooklynbrewery: {
+      label: "Brooklyn Brewery",
+      url: "http://brooklynbrewery.com/",
+      tags: ["nyc", "brooklyn", "beer", "pub", "cafe"],
+    },
+
+    sydneyoz: {
+      label: "Sydney, Australia",
+      url: "http://en.wikipedia.org/wiki/Sydney",
+      tags: ["australia", "sydney", "city"],
+    },
+
+    sydneybridgeclimb: {
+      label: "Sydney Harbour Bridge Tour",
+      url: "http://www.bridgeclimb.com",
+      tags: ["australia", "sydney", "tour"],
+    },
+
+    sydneybotanical: {
+      label: "Sydney Botanical Gardens",
+      url: "http://www.rbgsyd.nsw.gov.au/",
+      tags: ["australia", "sydney", "park"],
+    },
+
+    bluemountainsoz: {
+      label: "Blue Mountains",
+      url: "http://www.bluemts.com.au/",
+      tags: ["australia", "mountain", "park"],
+    },
+
+    barossaoz: {
+      label: "Australia - Barossa Valley Wine Region",
+      url: "http://www.barossa.com/",
+      tags: ["australia", "wine"],
+    },
+
+    melbournepz: {
+      label: "Melbourne",
+      url: "http://www.visitmelbourne.com/",
+      tags: ["australia", "melbourne", "city"],
+    },
+
+    melbourneapoz: {
+      label: "Melbourne - Albert Park / F1",
+      url: "http://en.wikipedia.org/wiki/Melbourne_Grand_Prix_Circuit",
+      tags: ["australia", "melbourne", "city", "park", "formula1"],
+    },
+
+    ggbridge: {
+      label: "Golden Gate Bridge",
+      url: "http://en.wikipedia.org/wiki/Golden_Gate_Bridge",
+      tags: ["sanfrancisco", "bridge"],
+    },
+
+    parispontneuf: {
+      label: "Paris - Pont Neuf",
+      url: "http://en.wikipedia.org/wiki/Pont_Neuf",
+      tags: ["paris", "city", "bridge"],
+    },
+
+    londontowerbridge: {
+      label: "London - Tower Bridge",
+      url: "http://en.wikipedia.org/wiki/Tower_Bridge",
+      tags: ["london", "city", "bridge"],
+    },
+
+    londonhyde: {
+      label: "London - Hyde Park",
+      url: "http://www.royalparks.gov.uk/Hyde-Park.aspx",
+      tags: ["london", "city", "park"],
+    },
+
+    mlblog1: {
+      label: "One Link, Many Paths, Clicking Choice! (MultiLink, Part 1)",
+      url:
+        "http://daniel.org/cafebucky/2012/02/26/one-link-many-paths-clicking-choice-multilink-part-1/",
+      tags: ["alap", "blog"],
+    },
+
+    mlblog2: {
+      label: "(MultiLink, Part 2) - where I introduce tags...",
+      url:
+        "http://daniel.org/cafebucky/2012/02/27/one-link-many-paths-clicking-choice-multilink-part-2/",
+      tags: ["alap", "blog"],
+    },
+
+    mlblog3: {
+      label: "(MultiLink, Part 3) - ID Includes and Tag Expressions...",
+      url:
+        "http://daniel.org/cafebucky/2012/03/01/one-link-many-paths-clicking-choice-multilink-part-3/",
+      tags: ["alap", "blog"],
+    },
+
+    sanfrancisco: {
+      label: "San Francisco (Wikipedia)",
+      url: "https://en.wikipedia.org/wiki/San_Francisco",
+      tags: ["sf"],
+    },
+
+    ferrybuilding: {
+      label: "SF Ferry Building",
+      url: "https://www.ferrybuildingmarketplace.com/",
+      tags: ["alap", "blog"],
+    },
+
+    acmebread: {
+      label: "Acme Bread",
+      url: "http://acmebread.com/locations",
+      tags: ["alap", "blog"],
+    },
+
+    sfchinatown: {
+      label: "SF Chinatown",
+      url: "http://www.sanfranciscochinatown.com/",
+      tags: ["alap", "blog"],
+    },
+    gbpizza: {
+      label: "Golden Boy Pizza",
+      url: "http://www.goldenboypizza.com/sanfrancisco.php",
+      tags: ["sf"],
+    },
+
+    gbpizza_photos: {
+      label: "Golden Boy Pizza (Photos)",
+      url: "http://www.goldenboypizza.com/gallery.php",
+      tags: ["sf"],
+    },
+
+    parkchalet: {
+      label: "Park Chalet",
+      url: "http://www.parkchalet.com/",
+      tags: ["alap", "blog"],
+    },
+  },
+};
---
+++ example.js
@@ -0,0 +1,13 @@
+import "./style.css";
+// our config object - in a dynamic project you would fetch
+// JSON from a server, and convert to an object...
+import { alapConfig } from "./Config.js";
+
+// our lib, locally...
+// import Alap from "../../src/index.js";
+
+// ...or, if you are using npm, this would be:
+import Alap from "alap";
+
+// pass the config object
+const alap = new Alap(alapConfig);
