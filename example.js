794ff2b158f8616d4598b2edce7449d40c97dcd3
Daniel Smith <javajoint@gmail.com>
2022-04-14 09:39:02 -0400

    merge

--- example.js
+++ example.js
@@ -1,17 +1,13 @@
-import "./style.css";
+import './style.css';
 // our config object - in a dynamic project you would fetch
 // JSON from a server, and convert to an object...
-import { alapConfig } from "./Config.js";
+import { alapConfig } from './Config.js';
 
 // our lib, locally...
 // import Alap from "./node_modules/alap/src/index.js";
-<<<<<<< HEAD
-import Alap from "../../src/index.js";
-=======
->>>>>>> 676b35a8d0a7ea817079dd3e1fcd0838706c6b8f
 
 // ...or, if you are using npm, this would be:
-//import Alap from "alap";
+import Alap from 'alap';
 
 // pass the config object
 const alap = new Alap(alapConfig);
